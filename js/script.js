dfdf84d369a9755058293b1b9833c57bc97e6ff1
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-07 16:34:06 +0100

    added js square

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-for (let i = 0; i < 99; i++) {
-    
-}
+let button = document.querySelector("#button");
+let grid = document.querySelector("#grid");
+
+button.addEventListener("click", 
+    function(){
+        
+        for (let i = 0; i < 100; i++) {
+           
+            let squareElement = document.createElement('div'); 
+            squareElement.classList.add('square');
+            grid.append(squareElement);
+            
+        }
+
+    }
+
+);
