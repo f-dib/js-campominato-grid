b5b7a8b45a312471bff95a52d722c3ea5b1d76a3
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-07 18:35:39 +0100

    exercise completed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 let button = document.querySelector("#button");
 let grid = document.querySelector("#grid");
 
+
 button.addEventListener("click", 
-    function(){
+    function(){ 
+
+        grid.innerHTML = "";
         
-        for (let i = 0; i < 100; i++) {
-           
-            let squareElement = document.createElement('div'); 
+        for (i = 0; i < 100; i++) {
+        
+            let squareElement = document.createElement('div');
             squareElement.classList.add('square');
+            squareElement.innerHTML = `${i + 1}`;
             grid.append(squareElement);
+
             
-        }
+            squareElement.addEventListener("click", 
+                function(){
+                    console.log(this.innerText);
+                    this.classList.toggle("activation");
+                })
 
-    }
+        }    
+       
+});
 
-);
